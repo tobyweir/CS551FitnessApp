f15cff32f8aa3d1d895ae0c7e4135c253f3a9cf0
poisonFrogz <charlie@thepiper.scot>
2026-04-07 20:32:21 +0100

    Start adding composables for Choosing your sex screen

--- app/src/main/java/com/example/cs551fitnessapp/ui/viewmodels/MembersViewModel.kt
+++ app/src/main/java/com/example/cs551fitnessapp/ui/viewmodels/MembersViewModel.kt
@@ -1,12 +1,16 @@
 package com.example.cs551fitnessapp.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
-import com.example.cs551fitnessapp.ui.viewmodels.states.MembersUiState
+//import com.example.cs551fitnessapp.ui.viewmodels.states.MembersUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
+
 class MembersViewModel : ViewModel() {
-    private val _uiState  = MutableStateFlow(MembersUiState(listOf()))
-    val uiState : StateFlow<MembersUiState> = _uiState.asStateFlow()
+
+    //private val _uiState  = MutableStateFlow(MembersUiState(listOf()))
+    //val uiState : StateFlow<MembersUiState> = _uiState.asStateFlow()
+
+
 }
