a0d44eabffa9d9e7b082cbbf0d7b4d9f04f0b94d
Toby Weir CS2020 <toby.weir.2020@uni.strath.ac.uk>
2026-04-09 12:54:30 +0100

    filled out the uiState, starting on the rest.

--- app/src/main/java/com/example/cs551fitnessapp/ui/viewmodels/MembersViewModel.kt
+++ app/src/main/java/com/example/cs551fitnessapp/ui/viewmodels/MembersViewModel.kt
@@ -1,12 +1,74 @@
 package com.example.cs551fitnessapp.ui.viewmodels
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-//import com.example.cs551fitnessapp.ui.viewmodels.states.MembersUiState
+import com.example.cs551fitnessapp.ui.screens.Member
+import com.example.cs551fitnessapp.ui.viewmodels.states.MembersUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import java.util.Date
 
 class MembersViewModel : ViewModel() {
-//    private val _uiState  = MutableStateFlow(MembersUiState(listOf()))
-//    val uiState : StateFlow<MembersUiState> = _uiState.asStateFlow()
+    val members = listOf<Member>(
+        Member(id = 0,
+            name ="John Smith" ,
+            joinDate = Date(2026 , 3 , 17) ,
+            endDate = null ,
+            status = "Active",),
+        Member( id = 1,
+            name ="Mike Smith" ,
+            joinDate = Date(2026 , 3 , 17) ,
+            endDate = null ,
+            status = "Active",),
+        Member(id = 2,
+            name ="Major Smith" ,
+            joinDate = Date(2026 , 3 , 17) ,
+            endDate = null ,
+            status = "Active",),
+        Member(id = 3,
+            name ="John Smith" ,
+            joinDate = Date(2026 , 3 , 17) ,
+            endDate = null ,
+            status = "Active",),
+        Member( id = 4,
+            name ="Mike Smith" ,
+            joinDate = Date(2026 , 3 , 17) ,
+            endDate = null ,
+            status = "Active",),
+        Member(id = 5,
+            name ="Major Smith" ,
+            joinDate = Date(2026 , 3 , 17) ,
+            endDate = null ,
+            status = "Active",),
+        Member(id = 6,
+            name ="John Smith" ,
+            joinDate = Date(2026 , 3 , 17) ,
+            endDate = null ,
+            status = "Active",),
+        Member( id = 7,
+            name ="Mike Smith" ,
+            joinDate = Date(2026 , 3 , 17) ,
+            endDate = null ,
+            status = "Active",),
+        Member(id = 8,
+            name ="Major Smith" ,
+            joinDate = Date(2026 , 3 , 17) ,
+            endDate = null ,
+            status = "Active",)
+    )
+    private val _uiState  = MutableStateFlow(MembersUiState(
+        listOf() ,
+        listOf() ,
+        includeActive = true ,
+        includeInactive = true,
+        includeNearlyFinished = true,
+        isError = false,
+    ))
+
+    val uiState : StateFlow<MembersUiState> = _uiState.asStateFlow()
+
+    var searchEntry by mutableStateOf("")
 }
