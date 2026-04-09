d7cb131d6ebd59194fb48702eac82e5380701488
poisonFrogz <charlie@thepiper.scot>
2026-04-09 10:47:16 +0100

    Changed implementation of toggle buttons to icon buttons, added functionality that if one is toggled the other is not

--- app/src/main/java/com/example/cs551fitnessapp/ui/screens/NewMemberSex.kt
+++ app/src/main/java/com/example/cs551fitnessapp/ui/screens/NewMemberSex.kt
@@ -8,8 +8,14 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -45,9 +51,17 @@ val circleMod = Modifier
     .border(4.dp,Color.LightGray,CircleShape)
     .background(Color.LightGray)
 
+val circleModFilled = Modifier
+    .clip(CircleShape)
+    .size(50.dp)
+    .border(4.dp,Color.DarkGray,CircleShape)
+    .background(Color.DarkGray)
+
 
 @Composable
 fun SexButton (modifier: Modifier = Modifier) {
+    var isToggledM by rememberSaveable { mutableStateOf(false) }
+    var isToggledF by rememberSaveable { mutableStateOf(false) }
     Row(
         modifier = Modifier
             .padding(24.dp)
@@ -58,13 +72,21 @@ fun SexButton (modifier: Modifier = Modifier) {
                 .padding(horizontal = 10.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-        // Attribution for readme: <a href="https://www.flaticon.com/free-icons/male" title="male icons">Male icons created by smashingstocks - Flaticon</a>
-            Image(
-                painter = painterResource(id = R.drawable.male),
-                contentDescription = "Male Icon",
-                contentScale = ContentScale.Inside,
-                modifier = circleMod
-            )
+
+            IconButton(
+                onClick = {
+                    isToggledM = !isToggledM
+                    if (isToggledF) isToggledM = false
+                }
+            ) {
+                // Attribution for readme: <a href="https://www.flaticon.com/free-icons/male" title="male icons">Male icons created by smashingstocks - Flaticon</a>
+                Image(
+                    painter = painterResource(R.drawable.male),
+                    contentDescription = if (isToggledM) "Male Button Filled" else "Male Button",
+                    contentScale = ContentScale.Inside,
+                    modifier = if (isToggledM) circleModFilled else circleMod
+                )
+            }
             Text(
                 text = "Male"
             )
@@ -75,18 +97,33 @@ fun SexButton (modifier: Modifier = Modifier) {
                 .padding(horizontal = 10.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            // Attribution for readme: <a href="https://www.flaticon.com/free-icons/woman" title="woman icons">Woman icons created by Freepik - Flaticon</a>
-            Image(
-                painter = painterResource(id = R.drawable.female),
-                contentDescription = "Female Icon",
-                contentScale = ContentScale.Inside,
-                modifier = circleMod
-            )
+
+            IconButton(
+                onClick = {
+                    isToggledF = !isToggledF
+                    if (isToggledM) isToggledF = false
+                }
+            ) {
+                // Attribution for readme: <a href="https://www.flaticon.com/free-icons/woman" title="woman icons">Woman icons created by Freepik - Flaticon</a>
+                Image(
+                    painter = painterResource(R.drawable.female),
+                    contentDescription = if (isToggledF) "Female Button Filled" else "Female Button",
+                    contentScale = ContentScale.Inside,
+                    modifier = if (isToggledF) circleModFilled else circleMod
+                )
+            }
             Text(
                 text = "Female"
             )
 
         }
+    }
+}
+
+@Composable
+fun SubmissionButtons (modifier: Modifier = Modifier) {
+    Column {
+
     }
 }
 
