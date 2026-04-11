40049e5bdb395fd4a51df9203a9ebd9390b8e133
Toby Weir CS2020 <toby.weir.2020@uni.strath.ac.uk>
2026-04-11 14:08:18 +0100

    fixed bug with today ui

--- app/src/main/java/com/example/cs551fitnessapp/ui/screens/TodayScreen.kt
+++ app/src/main/java/com/example/cs551fitnessapp/ui/screens/TodayScreen.kt
@@ -42,116 +42,83 @@ data class Session(
     val image: Int
 )
 
-
+val sessions = listOf(
+    Session(
+        "Alexander Bennett",
+        "10:00 AM",
+        "1 hr",
+        "2/10",
+        R.drawable.profile1
+    ),
+    Session(
+        "Jessica J.",
+        "02:00 PM",
+        "1 hr",
+        "8/10",
+        R.drawable.profile2
+    ),
+    Session(
+        "Olivia Turner",
+        "05:30 PM",
+        "1 hr",
+        "3/15",
+        R.drawable.profile3
+    ),
+    Session(
+        "Sophia M.",
+        "07:00 PM",
+        "2 hr",
+        "4/10",
+        R.drawable.profile4
+    )
+)
 @Composable
 fun TodayScreen(
     navController: NavController , modifier: Modifier
 ) {
-
     var selectedDate by remember {
         mutableStateOf("24")
     }
-
-
-    val sessions = listOf(
-
-        Session(
-            "Alexander Bennett",
-            "10:00 AM",
-            "1 hr",
-            "2/10",
-            R.drawable.profile1
-        ),
-
-        Session(
-            "Jessica J.",
-            "02:00 PM",
-            "1 hr",
-            "8/10",
-            R.drawable.profile2
-        ),
-
-        Session(
-            "Olivia Turner",
-            "05:30 PM",
-            "1 hr",
-            "3/15",
-            R.drawable.profile3
-        ),
-
-        Session(
-            "Sophia M.",
-            "07:00 PM",
-            "2 hr",
-            "4/10",
-            R.drawable.profile4
-        )
-    )
-
-
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = modifier.fillMaxSize()
     ) {
-
         DateSelector(
             selectedDate = selectedDate,
             onDateSelected = { selectedDate = it }
         )
-
-
         LazyColumn {
-
             items(sessions) {
-
                 SessionCard(
                     session = it,
                     navController = navController
                 )
             }
-
         }
-
     }
-
 }
-
-
 
 @Composable
 fun DateSelector(
     selectedDate: String,
     onDateSelected: (String) -> Unit
 ) {
-
     val dates = listOf("21","22","23","24","25","26","27")
-
-
     LazyRow(
-
         modifier = Modifier
             .fillMaxWidth()
             .background(Color(0xFFE3E8FF))
             .padding(vertical = 10.dp, horizontal = 12.dp),
-
         horizontalArrangement = Arrangement.spacedBy(10.dp)
-
     ) {
-
         items(dates) {
-
             DateItem(
                 day = it,
                 isSelected = it == selectedDate,
                 onClick = { onDateSelected(it) }
             )
-
         }
-
     }
-
 }
-
-
 
 @Composable
 fun DateItem(
@@ -159,9 +126,7 @@ fun DateItem(
     isSelected: Boolean,
     onClick: () -> Unit
 ) {
-
     Box(
-
         modifier = Modifier
             .clip(RoundedCornerShape(20.dp))
             .background(
@@ -170,127 +135,61 @@ fun DateItem(
             )
             .padding(horizontal = 18.dp, vertical = 12.dp)
             .clickable { onClick() }
-
     ) {
-
         Text(
             day,
             color =
                 if (isSelected) Color.White
                 else Color.Black
         )
-
     }
-
 }
-
-
 
 @Composable
 fun SessionCard(
     session: Session,
     navController: NavController
 ) {
-
     Card(
-
         modifier = Modifier
             .padding(horizontal = 16.dp, vertical = 8.dp),
-
         shape = RoundedCornerShape(20.dp),
-
         colors = CardDefaults.cardColors(
             containerColor = Color(0xFFDCE3F3)
         )
-
     ) {
-
         Row(
-
             modifier = Modifier.padding(16.dp),
-
             verticalAlignment = Alignment.CenterVertically
-
         ) {
-
-
             Image(
-
                 painter = painterResource(session.image),
-
                 contentDescription = null,
-
                 contentScale = ContentScale.Crop,
-
                 modifier = Modifier
                     .size(70.dp)
                     .clip(CircleShape)
-
             )
-
-
             Spacer(modifier = Modifier.width(16.dp))
-
-
-            Column(
-                modifier = Modifier.weight(1f)
-            ) {
-
-                Text(
-                    session.name,
+            Column(modifier = Modifier.weight(1f)) {
+                Text(session.name,
                     fontWeight = FontWeight.Bold,
-                    color = Color(0xFF2962FF)
-                )
-
+                    color = Color(0xFF2962FF))
                 Text("Start : ${session.start}")
                 Text("Duration : ${session.duration}")
-
-
                 Row {
-
                     Button(
-
-                        onClick = {
-                            navController.navigate(MemberPage(1)) //needs updated with member id
-                        },
-
+                        onClick = { navController.navigate(MemberPage(1)) }, //needs updated with member id
                         colors = ButtonDefaults.buttonColors(
-                            containerColor = Color(0xFF5C6BC0)
-                        )
-
-                    ) {
-
-                        Text("Info")
-
-                    }
-
-
+                            containerColor = Color(0xFF5C6BC0))
+                    ) { Text("Info") }
                     Spacer(modifier = Modifier.width(8.dp))
-
-
                     IconButton(onClick = { }) {
-
-                        Icon(
-                            Icons.Default.DateRange,
-                            contentDescription = null
-                        )
-
+                        Icon(Icons.Default.DateRange, contentDescription = null)
                     }
-
                 }
-
             }
-
-
-            AssistChip(
-                onClick = { },
-                label = {
-                    Text("session ${session.progress}")
-                }
-            )
-
+            AssistChip(onClick = { }, label = { Text("session ${session.progress}") })
         }
-
     }
-
 }
