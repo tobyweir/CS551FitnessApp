7378077f2627c5c46e324bd3b1ad3c97a03384c3
Toby Weir CS2020 <toby.weir.2020@uni.strath.ac.uk>
2026-04-17 16:34:57 +0100

    small change

--- app/src/main/java/com/example/cs551fitnessapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/cs551fitnessapp/ui/theme/Color.kt
@@ -3,11 +3,6 @@ package com.example.cs551fitnessapp.ui.theme
 import androidx.compose.ui.graphics.Color
 
 
-/*
---------------------------------------------------
-DEFAULT TEMPLATE COLORS (keep these unchanged)
---------------------------------------------------
-*/
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
@@ -17,15 +12,6 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-
-
-/*
---------------------------------------------------
-APP CUSTOM COLORS (used for dark/light theme)
---------------------------------------------------
-*/
-
-// LIGHT MODE
 val PrimaryLight = Color(0xFF2260ff)
 val SecondaryLight = Color(0xFFcad6ff)
 
